e7ce409742c050046a99560807fd26efb13dd8b9
ttsang2111 <ttsang2111@gmail.com>
2024-09-09 01:33:30 +0700

    move logo to navbar

--- src/app/(routes)/_components/navbar.tsx
+++ src/app/(routes)/_components/navbar.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import SidebarMobile from "./sidebar-mobile";
 import NavbarRoutes from "@/components/navbar-routes";
+import Logo from "./logo";
 
 const Navbar = () => {
     return (
-        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+        <div className="p-4 gap-x-4 border-b h-full flex items-center bg-white shadow-sm">
+            <Link href="/">
+                <Logo />
+            </Link>
             <SidebarMobile />
             <NavbarRoutes />
         </div>
     );
 }
 
-export default Navbar;
+export default Navbar;  
