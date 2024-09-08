79a9e0fcab01f955b09a7718da09fce3718c703b
ttsang2111 <ttsang2111@gmail.com>
2024-09-09 01:37:39 +0700

    change create UI

--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -3,7 +3,7 @@ import { usePathname } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { LogOut } from "lucide-react";
+import { LogOut, Plus } from "lucide-react";
 
 const NavbarRoutes = () => {
     const pathname = usePathname();
@@ -11,7 +11,7 @@ const NavbarRoutes = () => {
     const isCreatorPage = pathname?.startsWith('/creator');
     const isReaderPage = pathname?.startsWith('/reader');
     return (
-        <>
+        <>  
             <div className="flex gap-x-2 ml-auto">
                 {isCreatorPage || isReaderPage ?
                     (
@@ -25,6 +25,7 @@ const NavbarRoutes = () => {
                     (
                         <Link href="/creator/books">
                             <Button variant="ghost" className="font-semibold">
+                                <Plus className="h-4 w-4 mr-2" />
                                 Creator
                             </Button>
                         </Link>
