230616cd86bea510c81b2fad5a99d0ebeecc89c8
ttsang2111 <ttsang2111@gmail.com>
2024-09-09 01:37:06 +0700

    add navigation button to book name

--- src/app/reader/[bookId]/_components/book-sidebar.tsx
+++ src/app/reader/[bookId]/_components/book-sidebar.tsx
@@ -1,6 +1,8 @@
-import { Book, Chapter } from "@prisma/client";
+"use client";
 
+import { Book, Chapter } from "@prisma/client";
 import BookSidebarItem from "./book-sidebar-item";
+import { useRouter } from "next/navigation";
 
 interface BookSidebarProps {
     book: Book & {
@@ -8,18 +10,22 @@ interface BookSidebarProps {
     };
 }
 
-const BookSidebar = async (
+const BookSidebar = (
     {
         book, 
     }: BookSidebarProps
 ) => {
+    const router = useRouter();
+    const onClick = () => {
+        router.push(`/reader/${book.id}`);
+    }
     return (
         <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
-            <div className="p-8 flex flex-col border-b">
+            <button className="p-8 flex flex-col border-b" onClick={onClick}>
                 <h1 className="flex items-center justify-center font-semibold">
                     {book.name}
                 </h1>
-            </div>
+            </button>
             <div className="flex flex-col w-full">
                 {book.chapters.map((chapter) => (
                     <BookSidebarItem 
